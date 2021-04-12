1104e1f961c11dbabe62d2c7bdc1a5cfc03a87ed
Christian Mahnke <cmahnke@gmail.com>
2021-04-12 19:39:59 +0200

    Use better merge strategy

--- scripts/init/package.json.sh
+++ scripts/init/package.json.sh
@@ -6,15 +6,21 @@ if ! which jq &> /dev/null ; then
     echo "jq could not be found, exiting"
     exit 123
 fi
-if test -f package.json ; then
-    rm package.json
+
+if ! git diff --name-only --exit-code package.json ; then
+    git checkout package.json
 fi
 
-PACKAGE_FILES="$(find . -name package.hugo.json) $(find . -name package.json -depth 0 -size +0c )"
-PACKAGE_FILES=`echo $PACKAGE_FILES | tr '\n' ' '`
+#if test -f package.json ; then
+#    rm package.json
+#fi
+
+THEME_PACKAGE_FILES="$(find . -name package.hugo.json)"
+SITE_PACKAGE_FILE="$(find . -name package.json -depth 1 -size +0c)"
+PACKAGE_FILES=`echo $THEME_PACKAGE_FILES $SITE_PACKAGE_FILE | tr '\n' ' '`
 echo "Merging $PACKAGE_FILES"
 
-jq -s add $PACKAGE_FILES > package.json
+jq -s 'reduce .[] as $d ({}; . *= $d)' $PACKAGE_FILES > package.json
 if ! yarn install ; then
     ERR=$?
     cat package.json | jq -C .
