24fee3fa46a5ea5ec71915f150a6064e899e067e
Christian Mahnke <cmahnke@gmail.com>
2021-04-13 15:33:29 +0200

    Added translation, icons

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 find content/post/ -name info.json -exec dirname {} \; | xargs rm -r
-rm -rf docs/* node_modules/* resources/_gen 
+rm -rf docs/* node_modules/* resources/_gen
+rm -rf static/images/favicon*
