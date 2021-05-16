a20f69388ac4449af54439e94cd1d995cdb3056a
Christian Mahnke <cmahnke@gmail.com>
2021-05-16 21:23:02 +0200

    Print font config

--- scripts/github/setup-inkscape.sh
+++ scripts/github/setup-inkscape.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
-echo "Installing Inkscape"
-
-sudo add-apt-repository ppa:inkscape.dev/stable
-sudo apt update
-sudo apt install inkscape
-
 echo "Installing fonts"
 
 BASEDIR=$(dirname "$0")
 sudo cp $BASEDIR/../../static/fonts/*.ttf /usr/local/share/fonts
 fc-cache -f -v
+
+fc-list -v
+
+echo "Installing Inkscape"
+
+sudo add-apt-repository ppa:inkscape.dev/stable
+sudo apt update
+sudo apt install inkscape
