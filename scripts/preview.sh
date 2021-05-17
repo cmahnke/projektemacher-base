a135a98517f9ede78f38ede2b72ab1d2ca31390f
Christian Mahnke <cmahnke@gmail.com>
2021-05-17 18:44:09 +0200

    Made output format configurable

--- scripts/preview.sh
+++ scripts/preview.sh
@@ -2,6 +2,7 @@
 DEFAULT_SOURCE=content
 DEFAULT_FILENAME="ogPreview*.svg"
 DEFAULT_BACKGROUND=white
+DEFAULT_TARGETFORMAT=jpg
 
 if [ -z "$SOURCE" ] ; then
     SOURCE=$DEFAULT_SOURCE
@@ -14,6 +15,10 @@ if [ -z "$BACKGROUND" ] ; then
     BACKGROUND=$DEFAULT_BACKGROUND
 fi
 
+if [ -z "$TARGETFORMAT" ] ; then
+    TARGETFORMAT=$DEFAULT_TARGETFORMAT
+fi
+
 BASEDIR=$(dirname "$0")
 
 echo "Generating previews for $FILENAME from $SOURCE - background $BACKGROUND"
@@ -21,7 +26,10 @@ echo "Generating previews for $FILENAME from $SOURCE - background $BACKGROUND"
 # Generate Previews
 python3 $BASEDIR/preview.py
 
-
-find $SOURCE -name "$FILENAME" -print -exec bash -c 'inkscape "{}" --export-filename=$(dirname "{}")/$(basename -s .svg "{}").png; convert $(dirname "{}")/$(basename -s .svg "{}").png -background '$BACKGROUND' -flatten $(dirname "{}")/$(basename -s .svg "{}").jpg; rm "{}"' \;
+if [ $TARGETFORMAT != "png" ] ; then
+    find $SOURCE -name "$FILENAME" -print -exec bash -c 'inkscape "{}" --export-filename=$(dirname "{}")/$(basename -s .svg "{}").png; convert $(dirname "{}")/$(basename -s .svg "{}").png -background '$BACKGROUND' -flatten $(dirname "{}")/$(basename -s .svg "{}").'$TARGETFORMAT'; rm "{}"' \;
+else
+    find $SOURCE -name "$FILENAME" -print -exec bash -c 'inkscape "{}" --export-filename=$(dirname "{}")/$(basename -s .svg "{}").png' \;
+fi
 
 #; rm $(dirname "{}")/$(basename -s .svg "{}").png
