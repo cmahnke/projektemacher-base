4f5b6fc56702e2d687b807f5d713a158333e4999
Christian Mahnke <cmahnke@gmail.com>
2022-10-10 22:41:47 +0200

    Use requirements.txt for python

--- scripts/github/python-dependencies.sh
+++ scripts/github/python-dependencies.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
-PYTHON_DEPENDENCIES="pillow termcolor pyyaml toml icalendar"
+
+CTX_PATH="$(dirname $(realpath $0))"
+THEME_PATH=$(realpath --relative-to="$(cd $CTX_PATH/../../../..; echo $PWD)" $CTX_PATH/../..)
+
+PYTHON_DEPENDENCIES=`cat $THEME_PATH/requirements.txt  | tr '\n' ' '`
 
 echo "Installing Python modules $PYTHON_DEPENDENCIES"
 
 python -m pip install --upgrade pip
-pip install $PYTHON_DEPENDENCIES
+pip install -r $THEME_PATH/requirements.txt
