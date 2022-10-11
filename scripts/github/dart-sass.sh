0c70c0434eeccd954e448e58978c6628942cdd6c
Christian Mahnke <cmahnke@gmail.com>
2022-10-11 20:01:58 +0200

    added dart sass installer

---
+++ scripts/github/dart-sass.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+echo "Trying to set up Dart Sass"
+
+DARTSASS_VERSION=1.55.0
+OS="`uname | tr '[:upper:]' '[:lower:]'`"
+ARCH="`uname -m`"
+BIN_DIR=/usr/local/bin
+
+mkdir -p $BIN_DIR
+
+curl -LJO https://github.com/sass/dart-sass-embedded/releases/download/${DARTSASS_VERSION}/sass_embedded-${DARTSASS_VERSION}-${OS}-${ARCH}.tar.gz;
+
+tar -xvf sass_embedded-${DARTSASS_VERSION}-${OS}-${ARCH}.tar.gz
+
+mv sass_embedded/dart-sass-embedded $BIN_DIR
+
+rm -rf sass_embedded*;
+
+dart-sass-embedded --vers
