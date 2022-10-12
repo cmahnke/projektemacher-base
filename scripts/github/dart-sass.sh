49d31ba8231ebed6f8db6265ab4d6277996c765c
Christian Mahnke <cmahnke@gmail.com>
2022-10-12 08:24:24 +0200

    Added workflow for dependency installation

--- scripts/github/dart-sass.sh
+++ scripts/github/dart-sass.sh
@@ -3,10 +3,31 @@
 echo "Trying to set up Dart Sass"
 
 DARTSASS_VERSION=1.55.0
-OS="`uname | tr '[:upper:]' '[:lower:]'`"
-ARCH="`uname -m`"
 BIN_DIR=/usr/local/bin
+
+case "`uname`" in
+  'Darwin')
+    OS=macos
+    ;;
+  'Linux')
+    OS=linux
+    ;;
+esac
+
+case "`uname -m`" in
+  'x86-64')
+    ARCH=x64
+    ;;
+  'arm64')
+    ARCH=arm64
+    ;;
+  'arm')
+    ARCH=arm
+    ;;
+esac
+
 DARTSASS_URL="https://github.com/sass/dart-sass-embedded/releases/download/${DARTSASS_VERSION}/sass_embedded-${DARTSASS_VERSION}-${OS}-${ARCH}.tar.gz"
+
 
 echo "Downloading ${DARTSASS_URL}"
 
