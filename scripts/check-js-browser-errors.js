52cb9dad75323307e1767f56d7fe986c947a604f
Christian Mahnke <cmahnke@gmail.com>
2022-10-12 14:42:13 +0200

    updated check script

--- scripts/check-js-browser-errors.js
+++ scripts/check-js-browser-errors.js
@@ -1,14 +1,19 @@
 const puppeteer = require ('puppeteer')
 const fs = require('fs');
 const urlsFile = 'test-urls.txt';
-const localFilePrefix = 'file:./content/';
+const contentDir = 'docs'
+const localFilePrefix = 'file:./' + contentDir + '/';
 
-/*TODO: Load Urls  */
+if (!fs.existsSync(contentDir)) {
+    console.log('Directory %s doesn\'t exist!', contentDir);
+    process.exit(1);
+}
+
 var urls
-if (fs.existsSync(urlsFile )) {
+if (fs.existsSync(urlsFile)) {
     urls = fs.readFileSync(urlsFile).toString().split("\n");
 } else {
-  console.log(' File %s not found!', urlsfile);
+  console.log('File %s not found!', urlsFile);
   urls = ['/'];
 }
 
@@ -17,15 +22,27 @@ if (fs.existsSync(urlsFile )) {
         headless: true,
         devtools: false
     })
+    const page = await browser.newPage();
 
-    for (var url in urls) {
-        if (url == '/') {
-          url = 'index.html';
+    for (var i in urls) {
+        var localFile;
+        if (urls[i] == '/') {
+          localFile = localFilePrefix + 'index.html';
+        } else {
+          localFile = localFilePrefix + urls[i];
         }
 
-        const open = await page.goto ( localFilePrefix + url, { waitUntil: 'networkidle2', timeout: 0 });
+        if (!fs.existsSync(localFile)) {
+            console.log('Local file %s doesn\'t exist, skipping!', localFile);
+            continue;
+        }
+
+
+        console.log('Opening file %s', localFile);
+        const open = await page.goto(localFile, { waitUntil: 'networkidle2', timeout: 0 });
 
         const html = await page.content();
 
     }
-})
+    await browser.close();
+})();
