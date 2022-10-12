739df34ab0df6babd8cc20ad139ca4c1243f02b2
Christian Mahnke <cmahnke@gmail.com>
2022-10-12 08:49:10 +0200

    try to resolve different installation directories

--- scripts/github/python-dependencies.sh
+++ scripts/github/python-dependencies.sh
@@ -3,8 +3,14 @@
 CTX_PATH="$(dirname $(realpath $0))"
 THEME_PATH=$(realpath --relative-to="$(cd $CTX_PATH/../../../..; echo $PWD)" $CTX_PATH/../..)
 
+if [ -d $THEME_PATH ] ; then
+    SEARCH_PATH=$THEME_PATH
+else
+    SEARCH_PATH=$CTX_PATH
+fi
+
 python -m pip install --upgrade pip
-for REQUIREMENTS in `find $THEME_PATH/../../ -iname "requirements.txt"`
+for REQUIREMENTS in `find $SEARCH_PATH/../../ -iname "requirements.txt"`
 do
     PYTHON_DEPENDENCIES=`cat $REQUIREMENTS | tr '\n' ' '`
     echo "Installing Python modules '$PYTHON_DEPENDENCIES' from '$REQUIREMENTS'"
