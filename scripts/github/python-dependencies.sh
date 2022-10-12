51fd38de2583a29791f554719bc17d5ec12b2a58
Christian Mahnke <cmahnke@gmail.com>
2022-10-12 08:45:37 +0200

    Also search for python dependencies

--- scripts/github/python-dependencies.sh
+++ scripts/github/python-dependencies.sh
@@ -3,9 +3,10 @@
 CTX_PATH="$(dirname $(realpath $0))"
 THEME_PATH=$(realpath --relative-to="$(cd $CTX_PATH/../../../..; echo $PWD)" $CTX_PATH/../..)
 
-PYTHON_DEPENDENCIES=`cat $THEME_PATH/requirements.txt  | tr '\n' ' '`
-
-echo "Installing Python modules $PYTHON_DEPENDENCIES"
-
 python -m pip install --upgrade pip
-pip install -r $THEME_PATH/requirements.txt
+for REQUIREMENTS in `find $THEME_PATH/../../ -iname "requirements.txt"`
+do
+    PYTHON_DEPENDENCIES=`cat $REQUIREMENTS | tr '\n' ' '`
+    echo "Installing Python modules '$PYTHON_DEPENDENCIES' from '$REQUIREMENTS'"
+    pip install -r "$REQUIREMENTS"
+done
