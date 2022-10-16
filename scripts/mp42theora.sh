73e743f80ca4da761dd8d985eb58a6f148289b28
Christian Mahnke <cmahnke@gmail.com>
2022-10-16 15:31:06 +0200

    changed quality since theora produces bigger files

--- scripts/mp42theora.sh
+++ scripts/mp42theora.sh
@@ -9,5 +9,6 @@ do
     OUTPUT_FILE=`basename $INPUT mp4`
     OUTPUT="$OUTPUT_DIR/$OUTPUT_FILE"'ogv'
     echo "Converting '$INPUT' to '$OUTPUT'"
-    ffmpeg -i $INPUT -c: v libtheora -q: v 10 -c: a libvorbis -q: a 10 $OUTPUT
+    # Needed on Mac OS: https://github.com/homebrew-ffmpeg/homebrew-ffmpeg
+    ffmpeg -y -i $INPUT -c:v libtheora -q:v 7 -c:a libvorbis -q:a 4 $OUTPUT
 done
