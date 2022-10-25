b97d700a9800bee5e5581aff3f2a78bae644cdb6
Christian Mahnke <cmahnke@gmail.com>
2022-10-25 04:27:29 +0200

    Added vips as a dependency

--- scripts/github/setup-dependencies.sh
+++ scripts/github/setup-dependencies.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-RUN_DEPENDENCIES="bash imagemagick parallel rsync sshpass bash jq findutils libcairo2-dev pkg-config ffmpeg"
+RUN_DEPENDENCIES="bash imagemagick parallel rsync sshpass bash jq findutils libcairo2-dev pkg-config ffmpeg vips"
 
 echo "Installing $RUN_DEPENDENCIES"
 sudo apt-get install $RUN_DEPENDENCIES
