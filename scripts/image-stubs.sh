f0954730b501c69c494e10e30128326b0ffe5efb
Christian Mahnke <cmahnke@gmail.com>
2022-10-25 04:42:47 +0200

    Changed docker image tag

--- scripts/image-stubs.sh
+++ scripts/image-stubs.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 IMAGES=$(find content -maxdepth 4 -name '*.jxl')
-DOCKER_PREFIX="docker run -w ${PWD} -v ${PWD}:${PWD} ghcr.io/cmahnke/iiif-action:latest-jxl "
+DOCKER_PREFIX="docker run -w ${PWD} -v ${PWD}:${PWD} ghcr.io/cmahnke/iiif-action:latest-jxl-uploader "
 OUT_SUFFIX=".jpg"
 
 for IMAGE in $IMAGES
@@ -12,7 +12,7 @@ do
     echo "Processing $IMAGE..."
     if [ "$IMAGE_SUFFIX" == "jxl" ] ; then
         OUTPUT_FILE="$OUTPUT_DIR/$IMAGE_NAME$OUT_SUFFIX"
-        $CMD_PREFIX djxl -j "$IMAGE" "$OUTPUT_FILE"
+        $DOCKER_PREFIX djxl -j "$IMAGE" "$OUTPUT_FILE"
         echo "Saved $OUTPUT_FILE"
     fi
 
