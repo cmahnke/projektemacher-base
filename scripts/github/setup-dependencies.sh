aae85224187d03f3390523893ea6241804e389eb
Christian Mahnke <cmahnke@gmail.com>
2022-10-25 04:36:20 +0200

    Changed package name

--- scripts/github/setup-dependencies.sh
+++ scripts/github/setup-dependencies.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-RUN_DEPENDENCIES="bash imagemagick parallel rsync sshpass bash jq findutils libcairo2-dev pkg-config ffmpeg vips"
+RUN_DEPENDENCIES="bash imagemagick parallel rsync sshpass bash jq findutils libcairo2-dev pkg-config ffmpeg	libvips-tools"
 
 echo "Installing $RUN_DEPENDENCIES"
 sudo apt-get install $RUN_DEPENDENCIES
