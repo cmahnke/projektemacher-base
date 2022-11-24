8323fdd4a5c5b8f0b989f0d2ce25a5f2c4d26831
Christian Mahnke <cmahnke@gmail.com>
2022-11-24 15:25:46 +0100

    Don't use Docker explicitly  as dependency

--- scripts/github/setup-dependencies.sh
+++ scripts/github/setup-dependencies.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-RUN_DEPENDENCIES="bash imagemagick parallel rsync sshpass bash jq findutils libcairo2-dev pkg-config ffmpeg	libvips-tools docker"
+RUN_DEPENDENCIES="bash imagemagick parallel rsync sshpass bash jq findutils libcairo2-dev pkg-config ffmpeg	libvips-tools moby-cli"
 
 echo "Installing $RUN_DEPENDENCIES"
 sudo apt-get install $RUN_DEPENDENCIES
