3b27b9da574983cfeb22af14de69e8282e96cc98
Christian Mahnke <cmahnke@gmail.com>
2022-12-26 22:48:54 +0100

    Updated dependencies again

--- scripts/github/jxl-0.7.0.sh
+++ scripts/github/jxl-0.7.0.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo apt-get install libilmbase24 libopenexr24 libtcmalloc-minimal4 libhwy-dev 
+sudo apt-get install libopenexr-dev libtcmalloc-minimal4 libhwy-dev
 
 mkdir -p /tmp/jxl
 cd /tmp/jxl
@@ -9,9 +9,9 @@ wget https://github.com/libjxl/libjxl/releases/download/v0.7.0/jxl-debs-amd64-ub
 tar xzf jxl-debs-amd64-ubuntu-20.04-v0.7.0.tar.gz
 
 
-sudo dpkg -i libjxl_0.7_amd64.deb
-sudo dpkg -i jxl_0.7_amd64.deb
-sudo dpkg -i libjxl-dev_0.7_amd64.deb
+sudo apt install -f libjxl_0.7_amd64.deb
+sudo apt install -f jxl_0.7_amd64.deb
+sudo apt install -f libjxl-dev_0.7_amd64.deb
 
 cd ..
 rm -rf jxl
