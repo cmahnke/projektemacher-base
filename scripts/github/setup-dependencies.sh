b79dccf9c29877c375ba5ca6e7b379d6550010d7
Christian Mahnke <cmahnke@gmail.com>
2022-12-26 20:01:35 +0100

    Provide JXL Python bindings

--- scripts/github/setup-dependencies.sh
+++ scripts/github/setup-dependencies.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-RUN_DEPENDENCIES="bash imagemagick parallel rsync sshpass bash jq findutils libcairo2-dev pkg-config ffmpeg poppler-utils	libvips-tools moby-cli"
+RUN_DEPENDENCIES="bash imagemagick parallel rsync sshpass bash jq findutils libcairo2-dev pkg-config ffmpeg poppler-utils	libvips-tools moby-cli libjxl-dev"
 
 echo "Installing $RUN_DEPENDENCIES"
 sudo apt-get install $RUN_DEPENDENCIES
