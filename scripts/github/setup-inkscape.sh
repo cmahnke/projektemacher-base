b025760461305b9e88f6c89af4997385dd16a0e2
Christian Mahnke <cmahnke@gmail.com>
2023-01-02 13:26:02 +0100

    also install woff2 fonts

--- scripts/github/setup-inkscape.sh
+++ scripts/github/setup-inkscape.sh
@@ -1,8 +1,19 @@
 #!/bin/sh
 
+DECOMPRESS_DIR=/tmp/fonts/
+
 docker pull "ghcr.io/cmahnke/font-action:latest"
 
 echo "Installing fonts"
+
+mkdir -p "$DECOMPRESS_DIR"
+sudo find $BASEDIR/../../ -iname "static/fonts/*.woff2" -exec cp {} "$DECOMPRESS_DIR" \;
+for file in "$DECOMPRESS_DIR/*.woff2" ;
+do
+  docker run -w ${PWD} -v ${PWD}:${PWD} ghcr.io/cmahnke/font-action:latest /usr/local/bin/woff2_decompress "$file" ;
+done
+sudo find "$DECOMPRESS_DIR" -iname "*.ttf" -exec cp {} /usr/local/share/fonts \;
+
 
 BASEDIR=$(dirname "$0")
 #sudo cp $BASEDIR/../../static/fonts/*.ttf /usr/local/share/fonts
