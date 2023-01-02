9b5f90b1ab26a91ea6cb627606517c186e2c9822
Christian Mahnke <cmahnke@gmail.com>
2023-01-02 13:32:31 +0100

    Better loging for font setup

--- scripts/github/setup-inkscape.sh
+++ scripts/github/setup-inkscape.sh
@@ -10,14 +10,15 @@ mkdir -p "$DECOMPRESS_DIR"
 sudo find $BASEDIR/../../ -iname "static/fonts/*.woff2" -exec cp {} "$DECOMPRESS_DIR" \;
 for file in "$DECOMPRESS_DIR/*.woff2" ;
 do
+  echo "Decompressing $file using Docker"
   docker run -w ${PWD} -v ${PWD}:${PWD} ghcr.io/cmahnke/font-action:latest /usr/local/bin/woff2_decompress "$file" ;
 done
-sudo find "$DECOMPRESS_DIR" -iname "*.ttf" -exec cp {} /usr/local/share/fonts \;
+sudo find "$DECOMPRESS_DIR" -iname "*.ttf" -print -exec cp {} /usr/local/share/fonts \;
 
 
 BASEDIR=$(dirname "$0")
 #sudo cp $BASEDIR/../../static/fonts/*.ttf /usr/local/share/fonts
-sudo find $BASEDIR/../../ -iname "static/fonts/*.ttf" -exec cp {} /usr/local/share/fonts \;
+sudo find $BASEDIR/../../ -iname "static/fonts/*.ttf" -print -exec cp {} /usr/local/share/fonts \;
 fc-cache -f -v
 fc-list
 
