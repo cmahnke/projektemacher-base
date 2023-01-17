425acbfbebb5755590b64a5c2e73fc432fdae1e1
cmahnke <cmahnke@gmail.com>
2023-01-17 18:31:33 +0100

    Update setup-dependencies.sh

--- scripts/github/setup-dependencies.sh
+++ scripts/github/setup-dependencies.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-RUN_DEPENDENCIES="bash coreutils imagemagick parallel rsync sshpass bash jq findutils libcairo2-dev pkg-config ffmpeg poppler-utils	libvips-tools moby-cli patchelf wget"
+RUN_DEPENDENCIES="bash coreutils imagemagick parallel rsync sshpass bash jq findutils libcairo2-dev pkg-config poppler-utils	libvips-tools moby-cli patchelf wget"
 
 echo "Installing $RUN_DEPENDENCIES"
 sudo apt-get install $RUN_DEPENDENCIES
