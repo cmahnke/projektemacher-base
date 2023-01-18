64fd0f9d692ebe5267a42043f15b3e93e94f52b0
Christian Mahnke <cmahnke@gmail.com>
2023-01-18 08:36:04 +0100

    Update Packages sources bofore installing dependencies

--- scripts/github/python-dependencies.sh
+++ scripts/github/python-dependencies.sh
@@ -3,7 +3,8 @@
 PACKAGE_DEPENDENCIES="python3-matplotlib python3-numpy python3-pillow python3-yaml python3-pytoml python3-termcolor python3-wheel python3-cairosvg python3-icalendar cython3 python3-opencv"
 
 echo "Installing '$PACKAGE_DEPENDENCIES' from distro repository"
-sudo apt-get install $PACKAGE_DEPENDENCIES
+sudo apt-get update
+sudo apt-get install --fix-missing $PACKAGE_DEPENDENCIES
 
 CTX_PATH="$(dirname $(realpath $0))"
 THEME_PATH=$(realpath --relative-to="$(cd $CTX_PATH/../../../..; echo $PWD)" $CTX_PATH/../..)
--- scripts/github/setup-dependencies.sh
+++ scripts/github/setup-dependencies.sh
@@ -2,4 +2,5 @@
 RUN_DEPENDENCIES="bash coreutils imagemagick parallel rsync sshpass bash jq findutils libcairo2-dev pkg-config poppler-utils	libvips-tools moby-cli patchelf wget"
 
 echo "Installing $RUN_DEPENDENCIES"
+sudo apt-get update
 sudo apt-get install $RUN_DEPENDENCIES
