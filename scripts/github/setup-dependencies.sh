c7e221a78be42361bf9d22ab102acdf1a65b0458
Christian Mahnke <cmahnke@gmail.com>
2023-09-07 18:32:12 +0200

    lock docker

--- scripts/github/setup-dependencies.sh
+++ scripts/github/setup-dependencies.sh
@@ -2,6 +2,10 @@
 RUN_DEPENDENCIES="bash coreutils imagemagick parallel rsync sshpass bash jq findutils libcairo2-dev pkg-config poppler-utils	libvips-tools moby-cli patchelf wget"
 DOCKER_DEPENDENCIES="moby-buildx moby-cli moby-engine"
 
-echo "Installing $RUN_DEPENDENCIES"
 sudo apt-get update
+if [ "$( . /etc/lsb-release; echo $DISTRIB_RELEASE)" = "22.04" ] ; then
+  echo "Marking Docker packages for holding"
+  for pkg in docker-buildx-plugin docker-ce docker-ce-cli ; do sudo apt-mark hold $pkg; done
+fi
+echo "Installing $RUN_DEPENDENCIES"
 sudo apt-get install $RUN_DEPENDENCIES
