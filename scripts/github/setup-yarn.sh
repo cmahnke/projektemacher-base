d25590d529a33377b74bb595050235c13bd9d6be
Christian Mahnke <cmahnke@gmail.com>
2023-10-26 21:23:06 +0200

    Added yarn installer

---
+++ scripts/github/setup-yarn.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+sudo apt install curl
+curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt install yarn -y
