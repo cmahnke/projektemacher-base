53a75e1c55463441ebdd541c8988da185152b8eb
Christian Mahnke <cmahnke@gmail.com>
2023-10-26 21:44:16 +0200

    Fix installation

--- scripts/github/setup-yarn.sh
+++ scripts/github/setup-yarn.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
+echo "Installing Yarn"
 sudo apt install curl
 curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-sudo apt install yarn -y
+sudo apt-get update
+sudo apt-get install yarn -y
+
+yarn --version
+which yarn
