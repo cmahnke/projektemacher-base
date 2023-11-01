6fd31c37c06468fc795157f1f56ad9d619e37fcb
Christian Mahnke <cmahnke@gmail.com>
2023-11-01 23:20:22 +0100

    Added test

--- scripts/fonts.sh
+++ scripts/fonts.sh
@@ -2,14 +2,20 @@
 
 set -e -o pipefail
 
-( cd fonts && npm --no-audit install )
+if [ -z "$THEME_PATH" ] ; then
+  THEME_PATH=.
+fi
 
-BASE=fonts
+BASE=$THEME_PATH/fonts
 FONT_BASE="$BASE/node_modules"
 CSS_DIR="$BASE/out/css"
 FONT_DIR="$BASE/out/fonts"
+SITE_CSS=assets/css/fonts/
+SITE_FONTS=static/fonts/
 
-mkdir -p $CSS_DIR $FONT_DIR
+( cd $BASE && npm --no-audit install )
+
+mkdir -p $CSS_DIR $FONT_DIR $SITE_CSS $SITE_FONTS
 
 for FONT in `find "$FONT_BASE" -type d -depth 2` ;
 do
@@ -19,5 +25,7 @@ do
   sed -i '' -E 's/\.\/files/\.\/fonts/g' $CSS_DIR/$FONT_NAME.css
   cp $FONT/files/*.woff* $FONT_DIR
 done
-cp -n $CSS_DIR/* assets/css/fonts/
-cp -n $FONT_DIR/* static/fonts/
+echo "Copying CSS"
+cp -n $CSS_DIR/* $SITE_CSS ||/usr/bin/true
+echo "Copying Fonts"
+cp -n $FONT_DIR/* $SITE_FONTS
