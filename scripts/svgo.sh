72d839789a03869c132406b4302c5afdb74d8fc7
Christian Mahnke <cmahnke@gmail.com>
2024-01-02 13:46:16 +0100

    improved svgo handling

--- scripts/svgo.sh
+++ scripts/svgo.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 if [ -z "$IMAGES" ] ; then
   IMAGES=$(find content -name '*.svg')
 fi
@@ -9,29 +11,35 @@ if [ -z "`which jq`" ] ; then
   exit 2
 fi
 
-if [ -n "`jq -r '.scripts.svgo' package.json`" ] ; then
+if [ `jq -r '.scripts.svgo' package.json` ] ; then
   echo "Error: svgo command gets overwritten in package.json"
   exit 1
 fi
 
 if [ -n "$1" ] ; then
-  $OUTDIR="$1"
+  OUTDIR="$1"
   echo "Using $OUTDIR as output directory"
+fi
+
+if [ -z "$SVGO" ] ; then
+  SVGO=`yarn bin svgo`
 fi
 
 IFS=$(echo -en "\n\b")
 for IMAGE in $IMAGES
 do
     IMAGE_PREFIX=$(basename "$IMAGE" .svg)
-    TMP_FILE=${IMAGE_PREFIX}.tmp
+    if [ -z "$OUTDIR" ] ; then
+      TMP_FILE=${IMAGE_PREFIX}.tmp
+    else
+      TMP_FILE="$OUTDIR/`echo $IMAGE_PREFIX | tr '[:upper:]' '[:lower:]' | tr ' ' '-'`.svg"
+    fi
 
-    echo "Processing '$IMAGE'..."
-    yarn run svgo --config ./config/svgo.config.js -i "$IMAGE" -o "$TMP_FILE" --multipass
+    echo "Processing '$IMAGE' to '$TMP_FILE'"
+    $SVGO --config ./config/svgo.config.js -i "$IMAGE" -o "$TMP_FILE" --multipass
     if [ -z "$OUTDIR" ] ; then
       rm "$IMAGE"
-    else
-      IMAGE="$OUTDIR/$IMAGE_PREFIX.svg"
+      mv "$TMP_FILE" "$IMAGE"
     fi
-    mv "$TMP_FILE" "$IMAGE"
 
 done
