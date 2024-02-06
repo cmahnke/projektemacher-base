838be40452fd1d4093734237ac649b66fc7fdc74
Christian Mahnke <cmahnke@gmail.com>
2024-02-06 19:49:41 +0100

    Uses latest jxlpy

--- scripts/github/python-dependencies.sh
+++ scripts/github/python-dependencies.sh
@@ -16,6 +16,7 @@ else
 fi
 
 python -m pip install --upgrade pip
+set -e -o pipefail
 echo "Searching for requirements.txt in '$SEARCH_PATH'"
 for REQUIREMENTS in `find $SEARCH_PATH/../../ -iname "requirements.txt"`
 do
