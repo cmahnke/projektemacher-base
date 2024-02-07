25bb938b45b84ce7e69d378a4cf2f570ab83aace
Christian Mahnke <cmahnke@gmail.com>
2024-02-07 18:52:03 +0100

    Improved installation of jxlpy for mac

---
+++ scripts/install-jxlpy-mac.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+JXLPY="git+https://github.com/olokelo/jxlpy"
+
+CTX=`dirname $0`
+PYTHON=`$CTX/./find-python3.sh`
+PYTHON_PATH=`dirname $PYTHON`
+PYTHON_VERSION=`$PYTHON --version | cut -d' ' -f2 | cut -d'.' -f1,2`
+
+echo "Using $PYTHON, at $PYTHON_PATH, version $PYTHON_VERSION"
+
+brew install jpeg-xl
+
+$PYTHON_PATH/pip$PYTHON_VERSION install Pillow
+LDFLAGS="-L/opt/homebrew/lib/" CPPFLAGS="-I/opt/homebrew/include/" $PYTHON_PATH/pip$PYTHON_VERSION install $JXLPY
