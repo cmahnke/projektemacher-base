b3fba9bbba5959b081301dfa695265805ad2db88
Christian Mahnke <cmahnke@gmail.com>
2024-02-17 12:03:37 +0100

    Added simple setup phase linter for json

---
+++ scripts/json-lint.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+JSON_PREFIX=content
+CONVERT_SCRIPT="yarn run obj2gltf"
+
+set -e -o pipefail
+
+for FILE in `find $JSON_PREFIX -iname '*.json'`
+do
+  echo "Checking $FILE"
+  yarn run eslint -c themes/projektemacher-base/.eslintrc.yml $FILE
+done
