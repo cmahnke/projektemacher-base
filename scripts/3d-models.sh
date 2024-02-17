4e1d329f39a1a6702c96798c3ab632f5483b08f1
Christian Mahnke <cmahnke@gmail.com>
2024-02-17 08:58:11 +0100

    Apply metadata if exists

--- scripts/3d-models.sh
+++ scripts/3d-models.sh
@@ -3,13 +3,16 @@
 OBJ_PREFIX=content
 CONVERT_SCRIPT="yarn run obj2gltf"
 COMPRESS_SCRIPT="yarn run gltf-pipeline"
+METADATA_SCRIPT="yarn run gltf-transform xmp"
 DRACO=false
 
 for FILE in `find $OBJ_PREFIX -iname '*.obj'`
 do
     DIR=`dirname $FILE`
-    GLTF="$DIR/"`basename $FILE .obj`".gltf"
-    GLB="$DIR/"`basename $FILE .obj`".glb"
+    BASENAME=`basename $FILE .obj`
+    METADATA="$DIR/$BASENAME.json"
+    GLTF="$DIR/$BASENAME.gltf"
+    GLB="$DIR/$BASENAME.glb"
     echo "Converting $FILE in $DIR to $GLTF"
     if [ "$DRACO" = true ] ; then
       $CONVERT_SCRIPT -i "$FILE" -o "$GLTF"
@@ -17,4 +20,12 @@ do
     else
       $CONVERT_SCRIPT -i "$FILE" -b -o "$GLB"
     fi
+
+    if [ -r $METADATA ] ; then
+      echo "Adding metadata from $METADATA to $GLB"
+      $METADATA_SCRIPT $GLB "metadata-$GLB" --packet $METADATA
+      rm $GLB
+      mv "metadata-$GLB" $GLB
+    fi
+
 done
