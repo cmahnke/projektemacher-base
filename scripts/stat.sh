9e83d2be55eda5b78d73719292f99e9ee9e11107
cmahnke <cmahnke@gmail.com>
2024-05-19 12:46:17 +0200

    stat.sh aktualisieren

--- scripts/stat.sh
+++ scripts/stat.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
+mkdir -p data
 COUNT=`hugo list all future | tail -n +2 | wc -l | tr -d ' '`
 LATEST=`hugo list all future | sed -n 2p | cut -d ',' -f 4`
 jq -n --arg count "$COUNT" --arg latest "$LATEST" '$ARGS.named' > data/stats.json
