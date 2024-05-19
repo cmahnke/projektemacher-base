ad78ae753035e3a72be48301dbfd918b82139364
Christian Mahnke <cmahnke@gmail.com>
2024-05-19 17:55:49 +0200

    fix stats

--- scripts/stat.sh
+++ scripts/stat.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-COUNT=`hugo list all future | tail -n +2 | wc -l | tr -d ' '`
-LATEST=`hugo list all future | sed -n 2p | cut -d ',' -f 4`
+COUNT=`hugo list all | tail -n +2 | wc -l | tr -d ' '`
+LATEST=`hugo list all | tail -n +2 | cut -d ',' -f 4 | sort -r | head -1`
 jq -n --arg count "$COUNT" --arg latest "$LATEST" '$ARGS.named' > data/stats.json
