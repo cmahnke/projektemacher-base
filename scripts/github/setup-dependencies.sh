45dae290194aa3613cbd1ab2a7754e03541f1c74
Christian Mahnke <cmahnke@gmail.com>
2024-08-11 21:08:13 +0200

    Try to install JXL on Ubuntu 24.04

--- scripts/github/setup-dependencies.sh
+++ scripts/github/setup-dependencies.sh
@@ -7,5 +7,10 @@ if [ "$( . /etc/lsb-release; echo $DISTRIB_RELEASE)" = "22.04" ] ; then
   echo "Marking Docker packages for holding"
   for pkg in docker-buildx-plugin docker-ce docker-ce-cli ; do sudo apt-mark hold $pkg; done
 fi
+if [ "$( . /etc/lsb-release; echo $DISTRIB_RELEASE)" = "24.04" ] ; then
+  echo "Adding JXL to dependencies"
+  RUN_DEPENDENCIES="$RUN_DEPENDENCIES libjxl-tools"
+fi
+
 echo "Installing $RUN_DEPENDENCIES"
 sudo apt-get install $RUN_DEPENDENCIES
