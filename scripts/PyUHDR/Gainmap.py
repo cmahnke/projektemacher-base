fba43682f846be9df2f6f4d7a7da67169be5f2d5
Christian Mahnke <cmahnke@gmail.com>
2024-08-15 16:20:44 +0200

    Added python module

---
+++ scripts/PyUHDR/Gainmap.py
@@ -0,0 +1,39 @@
+import cv2 as cv
+import numpy as np
+import ffmpeg
+from PIL import Image
+
+def save_yuv(img, output_file, brightness=None, contrast=None):
+    if isinstance(img, (np.ndarray, np.generic)):
+        pass
+    elif isinstance(image, Image):
+        img = cv.cvtColor(np.array(img.convert('RGB')), cv.COLOR_RGB2BGR)
+
+    if len(img.shape) < 3:
+        img = cv.cvtColor(img, cv.COLOR_GRAY2BGR)
+    height, width, channels = img.shape
+
+    #args = ['ffmpeg', '-i', 'pipe:', '-filter:v', 'format=p010', name]
+    #cprint(f"$ {' '.join(args)}", 'yellow')
+
+    if brightness is not None or contrast is not None:
+        if brightness is not None and contrast is None:
+            eq = f"brightness={brightness}"
+        elif brightness is None and contrast is not None:
+            eq = f"contrast={contrast}"
+        elif brightness is not None and contrast is not None:
+            eq = f"brightness={brightness}:contrast={contrast}"
+        converter = (
+            ffmpeg.input('pipe:', format='rawvideo', pix_fmt='bgr24', s=f"{width}x{height}").filter("eq", eq)
+            .filter("format", "p010").output(output_file).overwrite_output().run_async(pipe_stdin=True)
+        )
+    else:
+
+        converter = (
+            ffmpeg.input('pipe:', format='rawvideo', pix_fmt='bgr24', s=f"{width}x{height}")
+            .filter("format", "p010").output(output_file).overwrite_output().run_async(pipe_stdin=True)
+        )
+    converter.communicate(input=img.astype(np.uint8).tobytes())
+    converter.stdin.close()
+    converter.wait()
+    return output_file
