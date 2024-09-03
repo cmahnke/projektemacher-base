94f3892ca169f7de86e75335167df0fa1225bca2
Christian Mahnke <cmahnke@gmail.com>
2024-09-03 20:25:47 +0200

    Added debug output

--- scripts/stat.sh
+++ scripts/stat.sh
@@ -5,3 +5,6 @@ mkdir -p data
 COUNT=`hugo list all | tail -n +2 | wc -l | tr -d ' '`
 LATEST=`hugo list all | tail -n +2 | cut -d ',' -f 4 | sort -r | head -1`
 jq -n --arg count "$COUNT" --arg latest "$LATEST" '$ARGS.named' > data/stats.json
+
+echo "Wrote data/stats.json:"
+cat data/stats.json
