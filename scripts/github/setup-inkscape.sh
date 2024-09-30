394fbdd58cfa9fc5f8f0cbd8facba293fd1eca71
Christian Mahnke <cmahnke@gmail.com>
2024-10-01 00:31:15 +0200

    Try to install more fonts

--- scripts/github/setup-inkscape.sh
+++ scripts/github/setup-inkscape.sh
@@ -10,9 +10,9 @@ echo "Installing fonts"
 mkdir -p "$DECOMPRESS_DIR"
 sudo find $BASEDIR -iname "static/fonts/*.woff2" -print -exec cp {} "$DECOMPRESS_DIR" \;
 cd $DECOMPRESS_DIR
-for file in "*.woff2" ;
+for file in ./*.woff2 ;
 do
-  echo "Decompressing $file using Docker"
+  echo "Decompressing font $file using Docker"
   docker run -w ${PWD} -v ${PWD}:${PWD} ghcr.io/cmahnke/font-action:latest /usr/local/bin/woff2_decompress "$file" ;
 done
 sudo find "$DECOMPRESS_DIR" -iname "*.ttf" -print -exec cp {} /usr/local/share/fonts \;
