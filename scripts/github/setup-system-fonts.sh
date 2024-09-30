6d667bf53e5bf3754353640169712a8969c867a8
Christian Mahnke <cmahnke@gmail.com>
2024-10-01 01:36:16 +0200

    Fix system fonts

--- scripts/github/setup-system-fonts.sh
+++ scripts/github/setup-system-fonts.sh
@@ -1,25 +1,44 @@
 #!/usr/bin/env bash
 
-BASEDIR="$(dirname "$0")/../../"
+BASEDIR="$(pwd)/$(dirname "$0")/../../"
 DECOMPRESS_DIR=./tmp/fonts/
+FONT_LIST=../fonts.lst
+JOBS=`nproc --all`
 
 docker pull "ghcr.io/cmahnke/font-action:latest"
 
 echo "Installing fonts"
 
 mkdir -p "$DECOMPRESS_DIR"
-sudo find $BASEDIR -iname "static/fonts/*.woff2" -print -exec cp {} "$DECOMPRESS_DIR" \;
+find $BASEDIR -path "*static/fonts/*.woff2" -print -exec cp {} "$DECOMPRESS_DIR" \;
 cd $DECOMPRESS_DIR
-for file in ./*.woff2 ;
+for file in `ls *.woff2` ;
 do
   echo "Decompressing font $file using Docker"
-  docker run -w ${PWD} -v ${PWD}:${PWD} ghcr.io/cmahnke/font-action:latest /usr/local/bin/woff2_decompress "$file" ;
+  echo $file >> $FONT_LIST
+  #docker run -w ${PWD} -v ${PWD}:${PWD} ghcr.io/cmahnke/font-action:latest /usr/local/bin/woff2_decompress "$file" ;
 done
-sudo find "$DECOMPRESS_DIR" -iname "*.ttf" -print -exec cp {} /usr/local/share/fonts \;
+cat $FONT_LIST | xargs -P $JOBS -n 1 docker run -w ${PWD} -v ${PWD}:${PWD} ghcr.io/cmahnke/font-action:latest /usr/local/bin/woff2_decompress
 cd $BASEDIR
-rm -rf $DECOMPRESS_DIR
+
+SYSTEM_FONT_DIR=/usr/local/share/fonts
+OS="`uname`"
+case "$OS" in
+  'Darwin')
+    echo "The following files would be copied to $SYSTEM_FONT_DIR"
+    find "$DECOMPRESS_DIR" -name "*.ttf" -print
+    find $BASEDIR -path "*static/fonts/*.ttf" -print
+    ;;
+  'Linux')
+    sudo find "$DECOMPRESS_DIR" -name "*.ttf" -print -exec cp {} $SYSTEM_FONT_DIR \;
+    sudo find $BASEDIR -path "*static/fonts/*.ttf" -print -exec cp {} $SYSTEM_FONT_DIR \;
+    fc-cache -f -v
+    fc-list
+    ;;
+esac
+
+
+
+#rm -rf $DECOMPRESS_DIR
 
 #sudo cp $BASEDIR/../../static/fonts/*.ttf /usr/local/share/fonts
-sudo find $BASEDIR -iname "static/fonts/*.ttf" -print -exec cp {} /usr/local/share/fonts \;
-fc-cache -f -v
-fc-list
--- scripts/image-postprocessing/convertScantailorOutToJXL.sh
+++ scripts/image-postprocessing/convertScantailorOutToJXL.sh
@@ -2,8 +2,16 @@
 
 IMAGES=$(find . -follow -path '*/out/*.tif' | grep -v cache)
 IFS=$'\n'
-JOBS=2
+JOBS=1
 JOBFILE=./vips-jobs
+
+EXIF=exiftool
+
+if ! command -v "$EXIF" 2>&1 >/dev/null
+then
+    echo "exiv2 not found, install it and make sure it's in PATH"
+    exit 1
+fi
 
 >$JOBFILE
 
@@ -19,16 +27,30 @@ do
     convert "$IMAGE" -interlace JPEG -quality 85% "$DIR/jpg/$FILENAME.jpg"
   fi
   if [ ! -f "$DIR/jxl/$FILENAME.jxl" ] ; then
-    	echo "Added generation of $DIR/jxl/$FILENAME.jxl to queue"
-	#convert "$IMAGE" -define jxl:effort=9 -define jxl:brotli_effort=11 -define jxl:distance=1 "$DIR/jxl/$FILENAME.jxl"
-#convert "$IMAGE" -define jxl:quality=90 -define jxl:brotli_effort=11 -define jxl:effort=9 "$DIR/jxl/$FILENAME.jxl"
-  #convert "$IMAGE" "$DIR/jxl/$FILENAME.jxl"
+      #exiv2 can currently only read
+      #PPIX=`exiv2 -g Exif.Image.XResolution "$IMAGE" | tr -s ' '  | cut -d ' ' -f 4`
+      #PPIY=`exiv2 -g Exif.Image.YResolution "$IMAGE" | tr -s ' '  | cut -d ' ' -f 4`
 
-    echo " \"$IMAGE\" \"$DIR/jxl/$FILENAME.jxl[distance=1,effort=9]\"" >> $JOBFILE
+      PPIX=`$EXIF -XResolution -S -n "$IMAGE" | cut -d ' ' -f 2`
+      PPIY=`$EXIF -YResolution -S -n "$IMAGE" | cut -d ' ' -f 2`
+
+    	echo "Added generation of $DIR/jxl/$FILENAME.jxl to queue"
+
+      #
+    	#convert "$IMAGE" -define jxl:effort=9 -define jxl:brotli_effort=11 -define jxl:distance=1 "$DIR/jxl/$FILENAME.jxl"
+      #convert "$IMAGE" -define jxl:quality=90 -define jxl:brotli_effort=11 -define jxl:effort=9 "$DIR/jxl/$FILENAME.jxl"
+      #convert "$IMAGE" "$DIR/jxl/$FILENAME.jxl"
+
+      # exiv2 can currently only read
+      #echo "'vips copy \"$IMAGE\" \"$DIR/jxl/$FILENAME.jxl[distance=1,effort=9]\" && $EXIF -M\"set Exif.Image.XResolution Rational $PPIX\" -M\"set Exif.Image.XResolution Rational $PPIY\" \"$DIR/jxl/$FILENAME.jxl\"'" >> $JOBFILE
+      echo "'vips copy \"$IMAGE\" \"$DIR/jxl/$FILENAME.jxl[distance=1,effort=9]\" && $EXIF -m \"$DIR/jxl/$FILENAME.jxl\" -resolutionunit=inches -XResolution=$PPIX -YResolution=$PPIY'" >> $JOBFILE
   fi
 done
 
 echo "Running JXL processing with $JOBS jobs, this may take a while"
-cat $JOBFILE | xargs -P $JOBS -n 2 vips copy
+cat $JOBFILE | xargs -P $JOBS -n 1 sh -c
 
 echo "Number of images: $(echo "$IMAGES"|wc -l|tr -d ' ')"
+
+echo "Removing backup files"
+find . -follow -path '*/jxl/*.jxl_original' -exec rm {} \;
