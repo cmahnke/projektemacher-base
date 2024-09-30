2c49f0211941577b8409ee18ec1bb15103a85918
Christian Mahnke <cmahnke@gmail.com>
2024-10-01 00:54:43 +0200

    Added test for font setuo

---
+++ scripts/github/setup-system-fonts.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+BASEDIR="$(dirname "$0")/../../"
+DECOMPRESS_DIR=./tmp/fonts/
+
+docker pull "ghcr.io/cmahnke/font-action:latest"
+
+echo "Installing fonts"
+
+mkdir -p "$DECOMPRESS_DIR"
+sudo find $BASEDIR -iname "static/fonts/*.woff2" -print -exec cp {} "$DECOMPRESS_DIR" \;
+cd $DECOMPRESS_DIR
+for file in ./*.woff2 ;
+do
+  echo "Decompressing font $file using Docker"
+  docker run -w ${PWD} -v ${PWD}:${PWD} ghcr.io/cmahnke/font-action:latest /usr/local/bin/woff2_decompress "$file" ;
+done
+sudo find "$DECOMPRESS_DIR" -iname "*.ttf" -print -exec cp {} /usr/local/share/fonts \;
+cd $BASEDIR
+rm -rf $DECOMPRESS_DIR
+
+#sudo cp $BASEDIR/../../static/fonts/*.ttf /usr/local/share/fonts
+sudo find $BASEDIR -iname "static/fonts/*.ttf" -print -exec cp {} /usr/local/share/fonts \;
+fc-cache -f -v
+fc-list
