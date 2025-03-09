b1abf3a60460cc50002141e1e7ef6a6121239734
Christian Mahnke <cmahnke@gmail.com>
2025-03-09 23:07:06 +0100

    Updated favicon generation

--- scripts/favicon.sh
+++ scripts/favicon.sh
@@ -6,16 +6,23 @@ echo "Generating favicons from $SOURCE"
 # See https://gist.github.com/pfig/1808188
 case "$SOURCE" in
   *.svg)
-    CMD="convert -density 2400 \"$SOURCE\" $OPTIONS static/images/favicon.png" ;;
+    CMD="convert -background none -density 2400 \"$SOURCE\" $OPTIONS static/images/favicon.png" ;;
   *)
     CMD="convert \"$SOURCE\" $OPTIONS static/images/favicon.png" ;;
 esac
 
 
 eval $CMD
-convert static/images/favicon.png -resize 256x256 -transparent white static/images/favicon-256.png
-convert static/images/favicon-256.png -resize 16x16 static/images/favicon-16.png
-convert static/images/favicon-256.png -resize 32x32 static/images/favicon-32.png
-convert static/images/favicon-256.png -resize 64x64 static/images/favicon-64.png
-convert static/images/favicon-256.png -resize 128x128 static/images/favicon-128.png
-convert static/images/favicon-16.png static/images/favicon-32.png static/images/favicon-64.png static/images/favicon-128.png static/images/favicon-256.png -colors 256 static/images/favicon.ico
+#
+convert static/images/favicon.png -resize 512x512 static/images/favicon-512.png
+convert static/images/favicon.png -resize 256x256 static/images/favicon-256.png
+convert static/images/favicon-256.png -resize 128x128 -transparent white static/images/favicon-128.png
+convert static/images/favicon-256.png -resize 64x64 -transparent white static/images/favicon-64.png
+convert static/images/favicon-256.png -resize 32x32 -transparent white static/images/favicon-32.png
+
+convert static/images/favicon-32.png static/images/favicon-64.png static/images/favicon-128.png -colors 256 static/images/favicon.ico
+
+convert static/images/favicon.png -resize 128x128 static/images/favicon-128.png
+convert static/images/favicon.png -resize 64x64 static/images/favicon-64.png
+convert static/images/favicon.png -resize 32x32 static/images/favicon-32.png
+convert static/images/favicon.png -resize 180x180 static/images/apple-touch-icon.png
