bff75512e652b810c14dafb38bd0869a94905592
Christian Mahnke <cmahnke@gmail.com>
2025-03-14 13:33:35 +0100

    Create 16x16 favigon

--- scripts/favicon.sh
+++ scripts/favicon.sh
@@ -30,8 +30,9 @@ convert static/images/favicon.png -resize 256x256 static/images/favicon-256.png
 convert static/images/favicon-256.png -resize 128x128 -transparent white static/images/favicon-128.png
 convert static/images/favicon-256.png -resize 64x64 -transparent white static/images/favicon-64.png
 convert static/images/favicon-256.png -resize 32x32 -transparent white static/images/favicon-32.png
+convert static/images/favicon-256.png -resize 16x16 -transparent white static/images/favicon-16.png
 
-convert static/images/favicon-32.png static/images/favicon-64.png static/images/favicon-128.png -colors 256 static/images/favicon.ico
+convert static/images/favicon-16.png static/images/favicon-32.png static/images/favicon-64.png static/images/favicon-128.png -colors 256 static/images/favicon.ico
 
 convert static/images/favicon.png -resize 128x128 static/images/favicon-128.png
 convert static/images/favicon.png -resize 64x64 static/images/favicon-64.png
