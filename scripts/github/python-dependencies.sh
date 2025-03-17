e1c6daad7ff4c93d7d53b9cf263a90036b091f47
Christian Mahnke <cmahnke@gmail.com>
2025-03-17 01:38:23 +0100

    Handle dir with space

--- scripts/github/python-dependencies.sh
+++ scripts/github/python-dependencies.sh
@@ -25,7 +25,7 @@ set -e
 echo "Searching for requirements.txt in '$SEARCH_PATH'"
 for REQUIREMENTS in `find $SEARCH_PATH/../../ -iname "requirements.txt" -not -path "./Source Files/*"`
 do
-    PYTHON_DEPENDENCIES=`cat $REQUIREMENTS | tr '\n' ' '`
+    PYTHON_DEPENDENCIES=`cat "$REQUIREMENTS" | tr '\n' ' '`
     echo "Installing Python modules '$PYTHON_DEPENDENCIES' from '$REQUIREMENTS'"
     pip install -r "$REQUIREMENTS"
 done
