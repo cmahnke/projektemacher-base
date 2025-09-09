c04f9a02a3b655da03e09da8be4ad67f1c0230a9
Christian Mahnke <194820+cmahnke@users.noreply.github.com>
2025-09-09 15:32:55 +0200

    Force install missing

--- scripts/github/jxl-0.11.1.sh
+++ scripts/github/jxl-0.11.1.sh
@@ -10,7 +10,7 @@ fi
 # This needs the `software-properties-common` package
 #sudo add-apt-repository -y universe
 #sudo apt-get update
-sudo apt-get install -y libstdc++6 libtcmalloc-minimal4 libgcc-s1 libc6 libilmbase25 libopenexr25 libhwy0 libhwy-dev libpng16-16 libbrotli1 libjpeg8 libgif7
+sudo apt-get install -y libstdc++6 libtcmalloc-minimal4 libgcc-s1 libc6 libhwy-dev libpng16-16 libbrotli1 libjpeg8 libgif7
 
 mkdir -p /tmp/jxl
 cd /tmp/jxl
@@ -21,6 +21,8 @@ tar xzf jxl-debs-amd64-ubuntu-24.04-v0.11.1.tar.gz
 sudo apt install -y -f ./libjxl_0.11.1_amd64.deb
 sudo apt install -y -f ./jxl_0.11.1_amd64.deb
 sudo apt install -y -f ./libjxl-dev_0.11.1_amd64.deb
+
+sudo apt-get -f install
 
 cd ..
 rm -rf jxl
