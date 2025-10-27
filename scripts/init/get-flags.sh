dfa7d598bddb4cab4866082c5d72e984c0ad3c7d
Christian Mahnke <194820+cmahnke@users.noreply.github.com>
2025-10-27 17:51:41 +0100

    Added flags to init

---
+++ scripts/init/get-flags.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+echo "Getting flags"
+
+SED=sed
+REALPATH=realpath
+OS="`uname`"
+case "$OS" in
+  'Darwin')
+    SED=gsed
+    REALPATH=grealpath
+    ;;
+  'Linux')
+    SED=sed
+    REALPATH=realpath
+    ;;
+esac
+echo "OS is '$OS', sed is '$SED', realpath is '$REALPATH'"
+
+CTX_PATH="$(dirname $($REALPATH $0))"
+THEME_PATH=$($REALPATH --relative-to="$(cd $CTX_PATH/../../../..; echo $PWD)" $CTX_PATH/../..)
+
+$THEME_PATH/scripts/flags.sh $THEME_PATH
