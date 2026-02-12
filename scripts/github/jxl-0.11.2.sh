1d8be71202e70fbfb840e3452252608725c77021
Christian Mahnke <194820+cmahnke@users.noreply.github.com>
2026-02-12 17:08:51 +0100

    Added icon

---
+++ scripts/github/jxl-0.11.2.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# See also https://doc.owncloud.com/server/next/admin_manual/installation/manual_installation/manual_imagick7.html
+
+if test -z "`which sudo`" ; then
+  apt-get update
+  apt-get install -y sudo wget
+fi
+
+# This needs the `software-properties-common` package
+#sudo add-apt-repository -y universe
+#sudo apt-get update
+sudo apt-get install -y libstdc++6 libtcmalloc-minimal4 libgcc-s1 libc6 libhwy-dev libpng16-16 libbrotli1 libjpeg8 libgif7
+
+mkdir -p /tmp/jxl
+cd /tmp/jxl
+
+wget https://github.com/libjxl/libjxl/releases/download/v0.11.2/jxl-debs-amd64-ubuntu-24.04-v0.11.2.tar.gz
+tar xzf jxl-debs-amd64-ubuntu-24.04-v0.11.2.tar.gz
+
+sudo apt install -y -f ./libjxl_0.11.2_amd64.deb
+sudo apt install -y -f ./jxl_0.11.2_amd64.deb
+sudo apt install -y -f ./libjxl-dev_0.11.2_amd64.deb
+
+sudo apt-get -f install
+
+cd ..
+rm -rf jxl
