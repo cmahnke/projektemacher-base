413ca1ba74ecb478a71d8d396e8936d847f2ff97
Christian Mahnke <194820+cmahnke@users.noreply.github.com>
2026-02-21 16:08:36 +0100

    Updated eslint infrastructure

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,52 +1,35 @@
 import globals from "globals";
-import jsParser from "eslint-plugin-json-es";
-import tomlParser from "toml-eslint-parser";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
 import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import eslintPluginToml from 'eslint-plugin-toml';
+import eslintPluginJsonc from 'eslint-plugin-jsonc';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
-
-export default [{
+export default [
+  {
     ignores: ["layouts/_default/*", "node_modules/*", "scripts/**/*.toml"],
-}, ...compat.extends("eslint:recommended", "plugin:toml/standard"), {
+  },
+  js.configs.recommended,
+  ...eslintPluginToml.configs["flat/standard"],
+  {
     languageOptions: {
-        globals: {
-            ...globals.browser,
-            ...globals.node,
-        },
+      globals: {
+          ...globals.browser,
+          ...globals.node,
+      },
 
-        ecmaVersion: 12,
-        sourceType: "module",
+      ecmaVersion: 12,
+      sourceType: "module",
     },
-
     rules: {
         "no-unused-vars": 1,
         "no-useless-assignment": 1,
         "toml/padding-line-between-tables": 0,
         "no-prototype-builtins": 0,
     },
-}, {
-    files: ["**/*.toml"],
-    languageOptions: {
-        parser: tomlParser
-    },
-}, ...compat.extends("plugin:eslint-plugin-json-es/recommended").map(config => ({
+  },
+  ...eslintPluginJsonc.configs["flat/recommended-with-jsonc"].map(config => ({
     ...config,
     files: ["**/*.gjson", "**/*.geojson", "**/*.json"],
-})), {
-    files: ["**/*.gjson", "**/*.geojson", "**/*.json"],
-
-    languageOptions: {
-        parser: jsParser,
-    },
-}, {
+  })), {
     files: ["scripts/*.cjs", "scripts/*.mjs"],
-}];
+  }
+];
