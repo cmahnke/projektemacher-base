44311ce743b7124cef903bad553f184f5c7f59ad
cmahnke <cmahnke@users.noreply.github.com>
2026-04-29 22:48:15 +0000

    deploy: 8d1f5f8a6e4632bdc2fa25230db306cdfd7fd820

---
+++ js/04819d2b3a7aff01fd8dd4f2d366cecf.351a5106ea50e90b505b134f94156221205cecce2044766c6c9328ec2d131691.js
@@ -0,0 +1,1 @@
+(()=>{function d(o,t){o.addEventListener("click",e=>{let a=t.value;navigator.clipboard.writeText(a).then(()=>{},()=>{console.warn("Failed to write to clipboard!")})})}window.addCopyToClipboard=d;})();
