8cef3de6fcc80501bfaa5afb77edaa99c5e5bd65
Christian Mahnke <194820+cmahnke@users.noreply.github.com>
2026-07-19 08:11:02 +0200

    Check befor trying duplicate installation

--- scripts/github/setup-dependencies.sh
+++ scripts/github/setup-dependencies.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
-set -e
+set -eu
 
-RUN_DEPENDENCIES="bash coreutils imagemagick parallel rsync sshpass bash jq findutils libcairo2-dev pkg-config poppler-utils	libvips-tools patchelf wget gifsicle libcairo2-dev libpango1.0-dev libgif-dev exiftool xmlstarlet librsvg2-bin"
+RUN_DEPENDENCIES="bash coreutils imagemagick parallel rsync sshpass jq findutils pkg-config poppler-utils	libvips-tools patchelf wget gifsicle libcairo2-dev libpango1.0-dev libgif-dev exiftool xmlstarlet librsvg2-bin"
 DOCKER_DEPENDENCIES="moby-buildx moby-cli moby-engine"
 
 sudo apt-get update
 if [ "$( . /etc/lsb-release; echo $DISTRIB_RELEASE)" = "22.04" ] ; then
   echo "Marking Docker packages for holding"
-  for pkg in docker-buildx-plugin docker-ce docker-ce-cli ; do sudo apt-mark hold $pkg; done
+  sudo apt-mark hold docker-buildx-plugin docker-ce docker-ce-cli
 fi
 if [ "$( . /etc/lsb-release; echo $DISTRIB_RELEASE)" = "24.04" ] ; then
   JXLPKG=libjxl-tools
-  if dpkg-query -Wf'${db:Status-abbrev}' "$JXLPKG" 2>/dev/null | grep -q '^i'; then
-    echo "The package '$JXLPKG' _is_ already installed!"
+  if dpkg -s "$JXLPKG" >/dev/null 2>&1; then
+    echo "'$JXLPKG' ist bereits installiert!"
   else
     echo "Adding JXL to dependencies"
     RUN_DEPENDENCIES="$RUN_DEPENDENCIES $JXLPKG"
@@ -20,4 +20,4 @@ if [ "$( . /etc/lsb-release; echo $DISTRIB_RELEASE)" = "24.04" ] ; then
 fi
 
 echo "Installing $RUN_DEPENDENCIES"
-sudo apt-get install $RUN_DEPENDENCIES
+sudo apt-get install -y $RUN_DEPENDENCIES
