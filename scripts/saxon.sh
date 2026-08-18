38b8b87c58defe49a4495044d8a01501b057e1f9
Christian Mahnke <194820+cmahnke@users.noreply.github.com>
2026-08-19 00:06:11 +0200

    Check more saxon loactions

--- scripts/saxon.sh
+++ scripts/saxon.sh
@@ -2,14 +2,27 @@
 
 set -e -o pipefail
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 SAXON_DIR="/opt/saxon"
+SAXON_SCRIPT="$SAXON_DIR/saxon"
 
 if [ -z "$SAXON" ] ; then
   SAXON=`which saxon`
 fi
 
+if [ -x "$SAXON_SCRIPT" ] ; then
+  SAXON=$SAXON_SCRIPT
+fi
+
 if [ -z "$SAXON" ] ; then
   SAXON="java -Xmx1024m -cp $SAXON_DIR/saxon.jar:$SAXON_DIR/xmlresolver.jar net.sf.saxon.Transform"
+fi
+
+if [ -z "$SAXON" ] ; then
+  echo "Trying to instal Saxon"
+  $SCRIPT_DIR/github/setup-xslt.sh
+  SAXON=$SAXON_SCRIPT
 fi
 
 if [ -z "$SAXON" ] ; then
