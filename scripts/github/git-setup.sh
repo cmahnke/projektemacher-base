06015a6720e4e911cb289b4aa4399c1e2ec33b8b
Christian Mahnke <194820+cmahnke@users.noreply.github.com>
2026-08-24 18:45:41 +0200

    Try to fix slow network on the runner

--- scripts/github/git-setup.sh
+++ scripts/github/git-setup.sh
@@ -3,3 +3,5 @@ set -e
 
 #git config --global http.version HTTP/1.1
 git config --global http.postBuffer 524288000
+git config --global http.lowSpeedLimit 0
+git config --global http.lowSpeedTime 600
